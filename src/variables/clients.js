0c71af34f6738679ed8e13c156e12adce1193851
Pankaj Patel <p.patel@holidaypirates.com>
2019-03-15 23:44:52 +0100

    client data structure

---
+++ src/variables/clients.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Microsoft',
+    address: 'USA',
+  },
+  {
+    id: 2,
+    name: 'SAP',
+    address: 'USA',
+  },
+  {
+    id: 3,
+    name: 'Bosch',
+    address: 'Germany',
+  }
+]
