64e967496e52a2b8b92f565e2a45a9add9a5ea3c
Nico Vignola <nicolantonio.vignola@getyourguide.com>
2019-03-15 23:09:11 +0100

    clean up template

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -42,8 +42,7 @@ class Dashboard extends Component {
       title: <span data-notify="icon" className="pe-7s-gift" />,
       message: (
         <div>
-          Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for
-          every web developer.
+          Notification message
         </div>
       ),
       level: level,
@@ -53,37 +52,6 @@ class Dashboard extends Component {
   }
   componentDidMount() {
     this.setState({ _notificationSystem: this.refs.notificationSystem });
-    var _notificationSystem = this.refs.notificationSystem;
-    var color = Math.floor(Math.random() * 4 + 1);
-    var level;
-    switch (color) {
-      case 1:
-        level = "success";
-        break;
-      case 2:
-        level = "warning";
-        break;
-      case 3:
-        level = "error";
-        break;
-      case 4:
-        level = "info";
-        break;
-      default:
-        break;
-    }
-    _notificationSystem.addNotification({
-      title: <span data-notify="icon" className="pe-7s-gift" />,
-      message: (
-        <div>
-          Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for
-          every web developer.
-        </div>
-      ),
-      level: level,
-      position: "tr",
-      autoDismiss: 15
-    });
   }
   componentDidUpdate(e) {
     if (
