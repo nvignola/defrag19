c5700b569e391c61c5e054f55f94b54e07d3f8a4
Nico Vignola <nicolantonio.vignola@getyourguide.com>
2019-03-17 12:24:50 +0100

    add glossary

---
+++ src/views/Glossary.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Grid, Row, Col } from "react-bootstrap";
+import DocImage from "assets/img/doc.png";
+import Glossary1 from "assets/img/1.png";
+import Glossary2 from "assets/img/2.png";
+import Glossary3 from "assets/img/3.png";
+import Glossary4 from "assets/img/4.png";
+
+import Card from "components/Card/Card.jsx";
+
+class Document extends Component {
+  render() {
+    return (
+      <div className="content">
+        <Grid fluid>
+          <Row>
+            <Col md={12}>
+              <img style={{maxWidth: '100%'}} src={Glossary1} alt=" " />
+              <img style={{maxWidth: '100%'}} src={Glossary2} alt=" " />
+              <img style={{maxWidth: '100%'}} src={Glossary3} alt=" " />
+              <img style={{maxWidth: '100%'}} src={Glossary4} alt=" " />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Document;
