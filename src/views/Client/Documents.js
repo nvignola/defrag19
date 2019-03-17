a561a869fdabf0facc645b9b2681b1e3ffbfa1f2
Pankaj Patel <p.patel@holidaypirates.com>
2019-03-17 10:07:44 +0100

    documents from rndpoints

--- src/views/Client/Documents.js
+++ src/views/Client/Documents.js
@@ -1,12 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link, Route } from "react-router-dom";
 import { Grid, Row, Col, Table } from "react-bootstrap";
-
+import gif from "assets/img/super_dino.gif";
 import Card from "components/Card/Card.jsx";
-import { documentsTHArray, documentsTDArray } from "variables/Variables.jsx";
 import Document from './Document';
 
+const DOCUMENTS_URL = 'http://file-upload.eu-west-1.elasticbeanstalk.com/documents';
+
 class Documents extends Component {
+  state = { documents: [] };
+
+  componentDidMount() {
+    fetch(DOCUMENTS_URL).then((documents) => this.setState({ documents }));
+  }
   render() {
     return (
       <div className="content">
@@ -34,42 +40,50 @@ class Documents extends Component {
                 ctTableFullWidth
                 ctTableResponsive
                 content={
-                  <Table striped hover>
-                    <thead>
-                      <tr>
-                        {documentsTHArray.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
+                  <Fragment>
+                  {
+                    this.state.documents.length
+                    ? <Table striped hover>
+                      <thead>
+                        <tr>
+                          {["ID", "Name", "Upload Date", "Actions"].map((prop, key) => {
+                            return <th key={key}>{prop}</th>;
+                          })}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.state.documents.map((prop, key) => {
+                          return (
+                            <tr key={key}>
+                              <td>{prop.id}</td>
+                              <td>{prop.name}</td>
+                              <td>{prop.uploadDate}</td>
+                              <td>
+                                <Link className="btn btn-primary btn-sm" to={`${this.props.match.url}/${key}`}>View Details</Link>
+                                &nbsp;
+                                <span className="btn btn-danger btn-sm">Delete</span>
+                              </td>
+                            </tr>
+                          );
                         })}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {documentsTDArray.map((prop, key) => {
-                        return (
-                          <tr key={key}>
-                            {prop.map((prop, key) => {
-                              return <td key={key}>{prop}</td>;
-                            })}
-                            <td key={'view'}>
-                              <Link className="btn btn-primary btn-sm" to={`${this.props.match.url}/${key}`}>View Details</Link>
-                              &nbsp;
-                              <span className="btn btn-danger btn-sm">Delete</span>
-                            </td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </Table>
+                      </tbody>
+                    </Table>
+                    : <div className="text-centered">
+                        <img src={gif} alt="..." style={{ width: '100%', display: 'inline-block' }}/>
+                    </div>
+                  }
+                  </Fragment>
                 }
               />
             </Col>
           </Row>
         </Grid>
-        {
-          documentsTDArray.map((prop, key) => <Route
-            key={key}
-            path={`${this.props.match.url}/${key}`}
-            render={(props) => <Document document={prop} {...props} />} />)
-        }
+        {(this.state.documents || []).map((doc) => <Route
+          key={doc.id}
+          path={`${this.props.match.url}/${doc.id}`}
+          render={(props) => <Document document={doc} {...props} />} 
+          />
+        )}
       </div>
     );
   }
